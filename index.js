2fca4ef02293b889b4da352159789a4968f7b53a
Pema Lama <pema.srp72@gmail.com>
2021-10-14 17:30:06 -0400

    commit tree aeacf0dc1799438cf50d8a5a8ca7d247d4112448

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 
+  function superbowlWin(arr){
+      const foundItem = arr.find( ({ name })=>  name === 'W' );
+      if(foundItem){
+          return foundItem.year;
+      }else{
+          return undefined;
+      }
+  }
+  
+
+ 
